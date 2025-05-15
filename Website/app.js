737b0bd194e9d88fed8d4274abf6f286c5d711ff
kurone-kito <krone@kit.black>
2025-05-16 00:17:45 +0900

    feat: import from the latest vpm-template

--- Website/app.js
+++ Website/app.js
@@ -6,31 +6,31 @@ import {
 const LISTING_URL = "{{ listingInfo.Url }}";
 
 const PACKAGES = eval(`({
-{{~ for package in packages ~}}
-  "{{ package.Name }}": {
-    name: "{{ package.Name }}",
-    displayName: "{{ if package.DisplayName; package.DisplayName; end; }}",
-    description: "{{ if package.Description; package.Description; end; }}",
-    version: "{{ package.Version }}",
-    author: {
-      name: "{{ if package.Author.Name; package.Author.Name; end; }}",
-      url: "{{ if package.Author.Url; package.Author.Url; end; }}",
+  {{~ for package in packages ~}}
+    "{{ package.Name }}": {
+      name: "{{ package.Name }}",
+      displayName: "{{ if package.DisplayName; package.DisplayName; end; }}",
+      description: "{{ if package.Description; package.Description; end; }}",
+      version: "{{ package.Version }}",
+      author: {
+        name: "{{ if package.Author.Name; package.Author.Name; end; }}",
+        url: "{{ if package.Author.Url; package.Author.Url; end; }}",
+      },
+      dependencies: {
+        {{~ for dependency in package.Dependencies ~}}
+          "{{ dependency.Name }}": "{{ dependency.Version }}",
+        {{~ end ~}}
+      },
+      keywords: [
+        {{~ for keyword in package.Keywords ~}}
+          "{{ keyword }}",
+        {{~ end ~}}
+      ],
+      license: "{{ package.License }}",
+      licensesUrl: "{{ package.LicensesUrl }}",
     },
-    dependencies: {
-      {{~ for dependency in package.Dependencies ~}}
-        "{{ dependency.Name }}": "{{ dependency.Version }}",
-      {{~ end ~}}
-    },
-    keywords: [
-      {{~ for keyword in package.Keywords ~}}
-        "{{ keyword }}",
-      {{~ end ~}}
-    ],
-    license: "{{ package.License }}",
-    licensesUrl: "{{ package.LicensesUrl }}",
-  },
-{{~ end ~}}
-})`);
+  {{~ end ~}}
+  })`);
 
 const setTheme = () => {
   const isDarkTheme = () =>
